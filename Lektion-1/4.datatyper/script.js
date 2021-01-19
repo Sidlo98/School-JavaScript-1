d03a6b0c2389a888c62044856cc65ab804171d24
Pontus Sidlo <Poppe9898@hotmail.com>
2021-01-19 14:53:55 +0100

    Objects

--- Lektion-1/4.datatyper/script.js
+++ Lektion-1/4.datatyper/script.js
@@ -98,12 +98,96 @@ let myName = 'Pontus Sidlo';
 let names = myName.split(' ');
 let firstName = names[0];
 let lastName = names[1];
-console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName}`);
+// console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName}`);
 
 let fullName = firstName + ' ' + lastName.toUpperCase();
-console.log(fullName);
+// console.log(fullName);
 
 let isActive = false;
-console.log(isActive);
+// console.log(isActive);
 
+
+firstName = firstName.concat(' är bäst');
+firstName = firstName.replace('bäst', 'KUNG');
+// console.log(firstName)
+
+
+
+/* 
+    NUMBERS
+    ----------------------------------
+
+    math operators:
+
+    +  lägg till
+    -  ta bort
+'   *  gångra
+    ** upphöjt i
+    /  dela
+    %  division remainder
+    ++ öka med 1
+    -- minska med 1
+
+    =  lika med 
+    += ha kvar det gamla och lägga till
+    -= ha kvar det gamla och ta bort från 
+'   *= ha kvar det gamla och gångra och lägg till 
+    /= ha kvar det gamla och dela och lägg till
+    %= ha kvar det gamla och division remainder
+*/
+
+let nr1 = 10;
+let nr2 = 2.5;
+
+// console.log(nr1,nr2);
+// console.log(nr1 + nr2);
+// console.log(nr1 - nr2);
+
+nr1 = nr1 + 1;
+nr1 += 1;
+nr1 ++;
+
+nr1 = nr1 - 1;
+nr1 -= 1; //kortaste sättet om man vill välja antal att justera med.
+nr1 --; //kortaste sättet men bara med 1.
+
+
+// console.log(nr1);
+
+//gånger
+// console.log(nr1 * nr2);
+//upphöjt i
+// console.log(nr1 ** nr2);
+//vad vlir kvar, division remainder
+// console.log(nr1 % 2); //får du noll i konsolen är nr1 ett jämt tal får det 1 så är det ett ojämt tal.
+
+let num = 1000000;
+
+// console.log(num.toFixed(2)); 
+// console.log(Math.round(num));   //avrundar
+// console.log(Math.floor(num));  //avrundar neråt
+// console.log(Math.ceil(num));  //avrundar uppåt
+// console.log(Math.floor(Math.random()*100));
+
+let region = 'sv-SE';
+
+// console.log(num.toLocaleString(region));
+
+let datatype;
+
+// let datatype1, datatype2, datatype3;
+
+console.log(typeof datatype);
+datatype = 'en text';
+console.log(typeof datatype);
+datatype = 3;
+console.log(typeof datatype);
+datatype = null; // DET HÄR ÄR EN BUGG I JAVASCRIPT
+console.log(typeof datatype);
+datatype = true;
+console.log(typeof datatype);
+datatype = {};  // DET HÄR ÄR ETT RIKTIGT OBJECT INTE SOM MED NULL
+console.log(typeof datatype);
+datatype = []; // DETTA ÄR EN ARRAY MEN RÄKNAS IN I DATATYPEN OBJECT
+console.log(typeof datatype);
 
---
+++ Lektion-1/5. object/script.js
@@ -0,0 +1,92 @@
+//OBJECT
+
+/* 
+  object innehåller ett eller flera värden
+
+  används för klasser, objekt och json (JavaScript Object Notation)
+  skrivs med ket:Value pairs
+  alla värden separeras av ett ,
+*/
+
+// person = {};
+// var person = {};
+// let person = {};
+// const person = {};
+
+// console.log(typeof person);
+
+
+//JS Object
+// const person = {
+//   firstName: 'Pontus',
+//   lastName: 'Sidlo'
+// };
+
+
+//JSON Object
+// const person = {
+//   "firstName": "Pontus",
+//   "lastName": "Pontus"
+// }
+
+// console.log(person);
+
+
+//konvertera till JSON
+// let json = JSON.stringify(person);
+// console.log(json);
+// console.log(typeof json);
+
+//konvertera från JSON till JS
+// const jsObject = JSON.parse(json);
+// console.log(jsObject)
+// console.log(typeof jsObject)
+
+const user = {
+  firstName: 'Pontus',
+  lastName: 'Sidlo',
+  age:22,
+  address:{
+    street: 'street',
+    nr: 5,
+    zipCode: '47945',
+    city: 'Göteborg'
+  },
+  phoneNumbers: [
+    '0735872940',
+    '0735987230'
+  ],
+  isActive: true,
+
+  fullName: function() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+}
+
+let key = 'lastName';
+
+// console.log(user);
+// console.log(user.firstName)
+// console.log(user.address.city)
+
+// console.log(user['firstName']);
+// console.log(user[key]);
+
+// console.log(user.fullName())
+
+let firstName = user.firstName;
+// console.log(firstName)
+
+// console.log(`användaren heter ${user.firstName} och bor i ${user.address.city}.`);
+
+user.firstName = 'Gleen';
+
+// console.log(`användaren heter ${user.firstName} och bor i ${user.address.city}.`);
+
+// user = 3; det här går inte när det är en const
+
+//lägga till eller ändra
+user.email = 'pontus.sidlo@outlook.se'
+
+// console.log(user.email)
